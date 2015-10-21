401c628be556029266e5b053cdd454324e3b0a95
Vygantas Lungis <VygantasLungis@gmail.com>
2015-10-21 22:11:52 +0300

    Initial project commit.

---
+++ Config.rb
@@ -0,0 +1,12 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'components/css'
+sass_dir = 'components/sass'
+images_dir = 'resources/images'
+javascripts_dir = 'components/javascript'
+fonts_dir = 'resources/fonts'
+relative_assets = true
+line_comments = true
+output_style = :compressed
+require 'breakpoint'
+require 'susy'
---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+gem 'sass', '~>3.4.18'
+gem 'compass', '~>1.1.0.alpha.3'
+gem 'breakpoint', '~>2.5.0'
+gem 'susy', '~>2.2.6'
